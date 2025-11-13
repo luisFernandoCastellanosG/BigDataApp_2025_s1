b2a66bab39c0b9f6194d370e1fa64e84eb5fae82
Luis Fernando Castellanos Guarín <LuisFC@corteconstitucional.gov.co>
2025-11-13 17:13:32 -0500

    limintacion de librerias a version 1
    limintacion de librerias a version 1

--- Helpers/__init__.py
+++ Helpers/__init__.py
@@ -2,4 +2,4 @@ from .mongoDB import MongoDB
 from .elastic import ElasticSearch
 from .funciones import Funciones
 
-__all__ = ['MongoDB', 'ElasticSearch', 'Funciones']
+__all__ = ['MongoDB',  'Funciones']
